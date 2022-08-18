43673c44ab6b46d42e895f4db666d9162a1b1ca2
Andriel Alexandre <103579864+andriel0@users.noreply.github.com>
2022-08-18 11:43:11 -0300

    Update API_Euro.py

--- API_Euro.py
+++ API_Euro.py
@@ -8,8 +8,8 @@ requisicao_dic = requisicao.json()
 cotacao_euro = requisicao_dic["EURBRL"]["bid"]
 
 #Tirando o account_sid e o token para preservar a conta
-account_sid = 'ACbc2a2e982da03f2fa3595275bd8a74c3'
-token = '265b8193772e9d0ca61aaa452dccccb2'
+account_sid = 'XXXX'
+token = 'YYYYY'
 
 client = Client(account_sid, token)
 
