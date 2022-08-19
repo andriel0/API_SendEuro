7ed7da32d8a25b280936976d091a336eae620407
Andriel Alexandre <103579864+andriel0@users.noreply.github.com>
2022-08-19 19:19:49 -0300

    Update API_Euro.py

--- API_Euro.py
+++ API_Euro.py
@@ -1,13 +1,19 @@
 from twilio.rest import Client
 import requests
-from datetime import datetime
+import time
 
-requisicao = requests.get("https://economia.awesomeapi.com.br/last/EUR-BRL")
+str_hora = "%d/%m/%y %H:%M:%S"
+hora = time.strftime(str_hora)
 
-requisicao_dic = requisicao.json()
-cotacao_euro = requisicao_dic["EURBRL"]["bid"]
+def mostrar_euro(link):
+    requisicao = requests.get(link)
+    requisicao_dic = requisicao.json()
+    cotacao_euro = float(requisicao_dic["EURBRL"]["bid"])
+    msg = 'Cotação Atualizada. {} \nEuro: R$ {}'.format(hora, cotacao_euro)
+    return msg
 
-#Tirando o account_sid e o token para preservar a conta
+euro = mostrar_euro("https://economia.awesomeapi.com.br/last/EUR-BRL")
+
 account_sid = 'XXXX'
 token = 'YYYYY'
 
@@ -15,11 +21,9 @@ client = Client(account_sid, token)
 
 numero_envio = '+12517328233'
 numero_recebe = '+5585996953190'
-msg = 'Cotação Atualizada. {}\nEuro: R$ {}'.format(datetime.now(), cotacao_euro)
-msg = str(msg)
 
 message = client.messages.create(
-    to = numero_recebe,
-    from_ = numero_envio,
-    body = msg)
+          to=numero_recebe,
+          from_=numero_envio,
+          body=euro)
 print(message.sid)
